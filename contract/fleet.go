ad25c0ba23683b9e7f9389da6f482f394a218e61
sc0vu <alk03073135@gmail.com>
2019-09-19 14:30:45 +0800

    Update key of access whitelist

--- contract/fleet.go
+++ contract/fleet.go
@@ -30,11 +30,15 @@ func DeviceWhitelistKey(addr []byte) []byte {
 }
 
 // AccessWhitelistKey returns storage key of access whitelist of givin address
-func AccessWhitelistKey(addr []byte) []byte {
+func AccessWhitelistKey(deviceAddr []byte, clientAddr []byte) []byte {
 	index := util.IntToBytes(AccessWhitelistIndex)
 	padIndex := util.PaddingBytesPrefix(index, 0, 32)
-	padAddr := util.PaddingBytesPrefix(addr, 0, 32)
+	padDeviceAddr := util.PaddingBytesPrefix(deviceAddr, 0, 32)
+	padClientAddr := util.PaddingBytesPrefix(clientAddr, 0, 32)
 	hash := sha3.NewKeccak256()
-	hash.Write(append(padAddr, padIndex...))
+	hash.Write(append(padDeviceAddr, padIndex...))
+	baseKey := hash.Sum(nil)
+	hash = sha3.NewKeccak256()
+	hash.Write(append(padClientAddr, baseKey...))
 	return hash.Sum(nil)
 }
