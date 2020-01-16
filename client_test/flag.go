198f2cf09e432300b5f4691b199e6d424347e5f6
sc0vu <alk03073135@gmail.com>
2020-01-16 15:14:12 +0800

    Add client_test for testing multiple connections

---
+++ client_test/flag.go
@@ -0,0 +1,32 @@
+// Diode Network Client
+// Copyright 2019 IoT Blockchain Technology Corporation LLC (IBTC)
+// Licensed under the Diode License, Version 1.0
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+// Config for client_test
+type Config struct {
+	Target          string
+	Conn            int
+	SocksServerAddr string
+	SocksServerHost string
+	SocksServerPort int
+}
+
+// parseFlag parse command line flags and return Config
+func parseFlag() *Config {
+	cfg := &Config{}
+
+	flag.StringVar(&cfg.Target, "target", "http://pi-taipei.diode", "test target")
+	flag.StringVar(&cfg.SocksServerHost, "socksd_host", "127.0.0.1", "host of socks server")
+	flag.IntVar(&cfg.SocksServerPort, "socksd_port", 1080, "port of socks server")
+	flag.IntVar(&cfg.Conn, "conn", 100, "total connection concurrently")
+
+	flag.Parse()
+	cfg.SocksServerAddr = fmt.Sprintf("%s:%d", cfg.SocksServerHost, cfg.SocksServerPort)
+	return cfg
+}
---
+++ client_test/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"sync"
+	"time"
+)
+
+func main() {
+	var config *Config
+	var proxyTransport *http.Transport = &http.Transport{
+		Proxy: http.ProxyURL(&url.URL{
+			Scheme: "socks5:",
+			Host:   "localhost:33",
+		}),
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	var wg sync.WaitGroup
+	config = parseFlag()
+	prox, _ := url.Parse(fmt.Sprintf("socks5://%s", config.SocksServerAddr))
+	proxyTransport.Proxy = http.ProxyURL(prox)
+	log.Printf("Start to connect %d times", config.Conn)
+	for i := 0; i < config.Conn; i++ {
+		wg.Add(1)
+		go func() {
+			client := &http.Client{
+				Transport: proxyTransport,
+			}
+			resp, err := client.Get(config.Target)
+			if err != nil {
+				log.Printf("Failed to get target: %s\n", err.Error())
+				wg.Done()
+				return
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Printf("Failed to read from body: %s\n", err.Error())
+				resp.Body.Close()
+				wg.Done()
+				return
+			}
+			log.Printf("Content: %s\n", string(body))
+			resp.Body.Close()
+			wg.Done()
+			return
+		}()
+	}
+	wg.Wait()
+}
