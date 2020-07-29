0aa7edb5fd08f740471bb49c8010efe2b2d33051
sc0vu <alk03073135@gmail.com>
2020-07-29 12:03:14 +0800

    Update doBNS

--- cmd/diode/diode.go
+++ cmd/diode/diode.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"regexp"
 	"runtime"
 	"runtime/pprof"
 	"sort"
@@ -33,6 +34,7 @@ var (
 	proxyServer     *rpc.ProxyServer
 	configAPIServer *ConfigAPIServer
 	pool            *rpc.DataPool
+	bnsPattern      = regexp.MustCompile(`^[0-9A-Za-z-]+$`)
 )
 
 func init() {
@@ -550,6 +552,15 @@ func doInitExp(cfg *config.Config, client *rpc.RPCClient) (status int) {
 	return
 }
 
+func isValidBNS(name string) (isValid bool) {
+	if len(name) < 7 || len(name) > 32 {
+		isValid = false
+		return
+	}
+	isValid = bnsPattern.Match([]byte(name))
+	return
+}
+
 func doBNS(cfg *config.Config, client *rpc.RPCClient) (status int) {
 	// register bns record
 	bn, _ := client.GetBlockPeak()
@@ -577,69 +588,71 @@ func doBNS(cfg *config.Config, client *rpc.RPCClient) (status int) {
 		status = 129
 		return
 	}
-
-	if len(registerPair) > 0 {
-		act, _ := client.GetValidAccount(uint64(bn), cfg.ClientAddr)
-		if act == nil {
-			nonce = 0
-		} else {
-			nonce = uint64(act.Nonce)
-		}
-		bnsName := registerPair[0]
-		var bnsAddr util.Address
-		if len(registerPair) > 1 {
-			bnsAddr, err = util.DecodeAddress(registerPair[1])
-			if err != nil {
-				printError("Invalid diode address", err)
-				status = 129
-				return
-			}
-		} else {
-			bnsAddr = cfg.ClientAddr
-		}
-		// check bns
-		obnsAddr, err := client.ResolveBNS(bnsName)
-		if err == nil {
-			if obnsAddr == bnsAddr {
-				printError("BNS name is already mapped to this address", err)
-				status = 129
-				return
-			}
-		}
-		// send register transaction
-		registerData, _ := dnsContract.Register(bnsName, bnsAddr)
-		ntx := edge.NewTransaction(nonce, 0, 10000000, contract.DNSAddr, 0, registerData, 0)
-		res, err := client.SendTransaction(ntx)
-		if err != nil {
-			printError("Cannot register blockchain name service: ", err)
-			status = 129
-			return
-		}
-		if !res {
-			printError("Cannot register blockchain name service: ", fmt.Errorf("server return false"))
-			status = 129
-			return
-		}
-		printLabel("Register bns: ", fmt.Sprintf("%s=%s", bnsName, bnsAddr.HexString()))
-		printInfo("Waiting for block to be confirmed - expect to wait 5 minutes")
-		for i := 0; i < 6000; i++ {
-			bn, _ = rpc.LastValid()
-			current, err := client.ResolveBNS(bnsName)
-			if err == nil && current == bnsAddr {
-				printInfo("Registered bns successfully")
-				return
-			}
-			for {
-				bn2, _ := rpc.LastValid()
-				if bn != bn2 {
-					break
-				}
-				time.Sleep(time.Millisecond * 100)
-			}
-		}
-		printError("Giving up to wait for transaction", fmt.Errorf("timeout after 10 minutes"))
+	bnsName := registerPair[0]
+	if !isValidBNS(bnsName) {
+		printError("Argument Error: ", fmt.Errorf("BNS name should be more than 7 or less than 32 characters (0-9A-Za-z-)"))
 		status = 129
+		return
 	}
+	act, _ := client.GetValidAccount(uint64(bn), cfg.ClientAddr)
+	if act == nil {
+		nonce = 0
+	} else {
+		nonce = uint64(act.Nonce)
+	}
+	var bnsAddr util.Address
+	if len(registerPair) > 1 {
+		bnsAddr, err = util.DecodeAddress(registerPair[1])
+		if err != nil {
+			printError("Invalid diode address", err)
+			status = 129
+			return
+		}
+	} else {
+		bnsAddr = cfg.ClientAddr
+	}
+	// check bns
+	obnsAddr, err := client.ResolveBNS(bnsName)
+	if err == nil {
+		if obnsAddr == bnsAddr {
+			printError("BNS name is already mapped to this address", err)
+			status = 129
+			return
+		}
+	}
+	// send register transaction
+	registerData, _ := dnsContract.Register(bnsName, bnsAddr)
+	ntx := edge.NewTransaction(nonce, 0, 10000000, contract.DNSAddr, 0, registerData, 0)
+	res, err := client.SendTransaction(ntx)
+	if err != nil {
+		printError("Cannot register blockchain name service: ", err)
+		status = 129
+		return
+	}
+	if !res {
+		printError("Cannot register blockchain name service: ", fmt.Errorf("server return false"))
+		status = 129
+		return
+	}
+	printLabel("Register bns: ", fmt.Sprintf("%s=%s", bnsName, bnsAddr.HexString()))
+	printInfo("Waiting for block to be confirmed - expect to wait 5 minutes")
+	for i := 0; i < 6000; i++ {
+		bn, _ = rpc.LastValid()
+		current, err := client.ResolveBNS(bnsName)
+		if err == nil && current == bnsAddr {
+			printInfo("Registered bns successfully")
+			return
+		}
+		for {
+			bn2, _ := rpc.LastValid()
+			if bn != bn2 {
+				break
+			}
+			time.Sleep(time.Millisecond * 100)
+		}
+	}
+	printError("Giving up to wait for transaction", fmt.Errorf("timeout after 10 minutes"))
+	status = 129
 
 	if len(lookupName) > 0 {
 		obnsAddr, err := client.ResolveBNS(lookupName)
