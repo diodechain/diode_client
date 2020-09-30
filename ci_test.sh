47df601f6e592db263f4b6884dcadd3c62277839
sc0vu <alk03073135@gmail.com>
2020-09-30 17:11:45 +0800

    Check whether the diode socks start in ci_test.sh

--- ci_test.sh
+++ ci_test.sh
@@ -6,9 +6,24 @@
 ./diode_race_test -diodeaddrs asia.testnet.diode.io:41046 -e2e=false socksd > /dev/null & 
 
 diode_pid=$!
-echo "Start diode-cli pid: $diode_pid and sleep 3 seconds"
+echo "Start diode-cli pid: $diode_pid"
 
-sleep 3
+# check whether the diode socks start
+# client should connect to valid network in 9 seconds
+for i in 3
+do
+    res=$(lsof -i4:1080)
+    if [ ${#res} -ge 5 ]
+    then
+        break
+    fi
+    res=$(lsof -i6:1080)
+    if [ ${#res} -ge 5 ]
+    then
+        break
+    fi
+    sleep 3 
+done
 
 curl -v --socks5-hostname localhost:1080 pi-taipei.diode.link
 ret=$?
