46b92b12ab105100e9b1c4585be22706ed318ce4
sc0vu <alk03073135@gmail.com>
2020-10-27 23:41:56 +0800

    Update ECMULT_WINDOW_SIZE and ECMULT_GEN_PREC_BITS

--- crypto/secp256k1/secp256.go
+++ crypto/secp256k1/secp256.go
@@ -14,8 +14,8 @@ package secp256k1
 #define USE_SCALAR_8X32
 #define USE_SCALAR_INV_BUILTIN
 #define NDEBUG
-#define ECMULT_WINDOW_SIZE 24
-#define ECMULT_GEN_PREC_BITS 8
+#define ECMULT_WINDOW_SIZE 15
+#define ECMULT_GEN_PREC_BITS 4
 #include "./libsecp256k1/src/secp256k1.c"
 #include "./libsecp256k1/src/modules/recovery/main_impl.h"
 #include "ext.h"
