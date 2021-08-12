7f02e73175e712d5cff0875b39bb6d7c600dceee
Dominic Letz <dominic@diode.io>
2021-08-12 18:31:29 +0200

    Added todo

--- rpc/tcpconn.go
+++ rpc/tcpconn.go
@@ -12,6 +12,13 @@ import (
 func configureTcpConn(tcpConn *net.TCPConn) {
 	tcpConn.SetKeepAlive(true)
 	tcpConn.SetKeepAlivePeriod(time.Minute)
-	tcpConn.SetReadBuffer(1000000)
-	tcpConn.SetWriteBuffer(1000000)
+	// TODO: These buffer settings are actually not applied!
+	//       They need to set before the TCP connection is connected
+	//       atm using `sysctl net.ipv4.tcp_rmem="4096        531072  64291456"`
+	//
+	// TODO: Think dig into impl. it seems setting these will also
+	//       disable linux auto-tune for sockets so now they are
+	//       skipped
+	// tcpConn.SetReadBuffer(1000000)
+	// tcpConn.SetWriteBuffer(1000000)
 }
