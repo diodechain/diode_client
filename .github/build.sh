d3e7d0f91254e4bf4eb9b6a23dd62af40046f107
Dominic Letz <dominic@diode.io>
2023-07-20 15:33:34 +0800

    Updating CI builds

--- .github/build.sh
+++ .github/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 docker build -t pi-arm32 -f pi-arm32.dockerfile .. && \
     docker run --rm --entrypoint cat pi-arm32 /build/diode_client/diode_linux_amd64.zip > diode_linux_arm.zip && \
     docker build -t pi-arm64 -f pi-arm64.dockerfile .. && \
