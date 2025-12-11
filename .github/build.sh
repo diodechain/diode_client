93431112167007b962bcd8a26287e8262ec35340
Dominic Letz <dominic@diode.io>
2025-12-11 21:59:50 +0100

    wip

--- .github/build.sh
+++ .github/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
-docker build -t pi-arm32 -f pi-arm32.dockerfile .. && \
-    docker run --rm --entrypoint cat pi-arm32 /build/diode_client/diode_linux_amd64.zip > diode_linux_arm.zip && \
-    docker build -t pi-arm64 -f pi-arm64.dockerfile .. && \
-    docker run --rm --entrypoint cat pi-arm64 /build/diode_client/diode_linux_amd64.zip > diode_linux_arm64.zip
+docker build -t pi-arm32 -f pi-arm32.dockerfile .. 
+docker run --rm --entrypoint cat pi-arm32 /build/diode_client/diode_linux_amd64.zip > diode_linux_arm.zip
+
+docker build -t pi-arm64 -f pi-arm64.dockerfile .. 
+docker run --rm --entrypoint cat pi-arm64 /build/diode_client/diode_linux_amd64.zip > diode_linux_arm64.zip
+
+docker build -t bullseye-amd64 -f bullseye-amd64.dockerfile .. 
+docker run --rm --entrypoint cat bullseye-amd64 /build/diode_client/diode_linux_amd64.zip > diode_linux_amd64_bullseye.zip
---
+++ .github/bullseye-amd64.dockerfile
@@ -0,0 +1,36 @@
+FROM debian:bullseye
+
+ENV GOOS=linux
+ENV CGO_ENABLED=1
+
+RUN apt-get update -y && \
+    apt-get install -y git && \
+    apt-get install -y build-essential && \
+    apt-get install -y pkg-config && \
+    apt-get install -y upx && \
+    apt-get install -y zip && \
+    apt-get install -y wget
+
+# install golang
+RUN echo "Build and install golang......"
+ENV GOFILE=go1.25.3.linux-amd64.tar.gz
+RUN wget https://dl.google.com/go/$GOFILE && \
+    [ "0335f314b6e7bfe08c3d0cfaa7c19db961b7b99fb20be62b0a826c992ad14e0f" = "$(sha256sum $GOFILE | cut -d ' ' -f1)" ] && \
+    tar -xvf $GOFILE
+RUN mv go /usr/local
+ENV GOROOT "/usr/local/go"
+RUN mkdir /go
+ENV GOPATH "/go"
+ENV PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
+ENV EXTRA_TAGS=no_tray
+
+RUN mkdir build
+WORKDIR /build
+
+# OpenSSL Settings
+RUN mkdir diode_client
+WORKDIR /build/diode_client
+
+COPY . .
+RUN make openssl
+RUN make archive
